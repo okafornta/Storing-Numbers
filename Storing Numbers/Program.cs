8a9e3434e2fabe20f99965c46fa99cab3bcdb764
Okafornta <hillaryonia@gmail.com>
2022-11-21 11:58:06 +0100

    Add project files.

---
+++ Storing Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿/* TASK:  
+ *  I will create (declare) a variable called number. Afterward, I will store some number
+    in it. Finally, I will display the variable’s value to the user.
+ */
+
+// Variable for storing number (with initial value)
+int number = 20;
+
+// Output of value of the variable
+Console.WriteLine("Value of the variable: " + number);
+
+// Waiting for Enter
+Console.ReadLine();
+
+// NOTE: Do not forget, numbers are entered without quotes.
